bdd1190d376fc324870990833e3f79024d307ef9
iagost <iagost@outlook.com.br>
2019-11-20 23:18:19 -0300

    redis and mail

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,13 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController{
 
@@ -36,7 +39,7 @@ class AppointmentController{
     return res.json(appointments);
   }
 
-  async store (req, res){
+  async store(req, res){
 
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -98,6 +101,48 @@ class AppointmentController{
     })
 
     return res.json(appointment);
+  }
+
+  async delete(req, res){
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    if(appointment.user_id != req.userId){
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment."
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if(isBefore(dateWithSub, new Date())){
+      return res.status(401).json({
+        error: 'You can only cancel appointements 2 hours in advance.',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
+
+    await Queue.add(CancellationMail.key, {
+      appointment,
+    });
+
+    return res.json(appointment);
+
   }
 
 }
